dd148981257151677e21e85dc778dbc45b8b52f9
Anifrontend <131538227+Anifrontend@users.noreply.github.com>
2025-07-11 13:06:48 +0530

    Initial commit: Added hello.py and set up Python learning environment

---
+++ hello.py
@@ -0,0 +1,1 @@
+print(" Heya Aniket is in the game")
---
+++ practice1.py
@@ -0,0 +1,18 @@
+
+# A practice of Arithmatic Operator
+a = 15
+b = 4
+
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a / b = {a / b}")
+print(f"a % b = {a % b}")
+print(f"a // b = {a // b}")
+ 
+# comparison
+
+print(f"a == b {a == b}")
+print(f"a != b {a != b}")
+print(f"a < b {a < b}")
+print(f"a > b {a > b}")
+
