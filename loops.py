a91b969c76b0491264fb8723289c2afb2121089b
Anifrontend <131538227+Anifrontend@users.noreply.github.com>
2025-07-16 13:24:56 +0530

    Completed Day 3 of loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+# we have two kind of loops in it and both are necessary to understand
+# 1. while
+
+# while True : 
+   # print("hello")
+
+    
+# but the above syntax doesnt contains any end of the loop so it will go on till infinity.
+
+a = 1
+while a <= 9 :
+    print("hello")
+    a += 1
--- practice1.py
+++ practice1.py
@@ -16,3 +16,28 @@ print(f"a != b {a != b}")
 print(f"a < b {a < b}")
 print(f"a > b {a > b}")
 
+# variable making
+
+city = "Hyderabad"
+temperature = 20
+is_raining = True
+
+print(f"city {city}, temperature {temperature}, raining {is_raining}")
+
+# dictionary & sets
+
+mydict = {
+    "name" : "Anuj",
+    "SirName" : "Sharma",
+    "info" : {
+        "Age" : 22,
+        "Category" : "gen",
+    }
+}
+
+#print(mydict)
+print(mydict["name"])
+print(list(mydict.keys()))
+
+# if we do *print(mydict["name2"])* run terminal shows us error but if we put *print(mydict.get(name2))* then it will show "NOne" and the second approach will be a better approach always because first one will stop next other line codes, that make s your code unstable.
+
