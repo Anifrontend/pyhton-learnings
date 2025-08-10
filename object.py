3074f57b66b6d7f2553b1c2b52a27efdb98ff69f
Anifrontend <131538227+Anifrontend@users.noreply.github.com>
2025-08-10 12:47:08 +0530

    Completed Day 5 exercises: Dictionaries and Sets

---
+++ object.py
@@ -0,0 +1,9 @@
+person = {
+    "name" : "Aniket",
+    "course" : "MCA",
+    "boy" : "playBoy"
+}
+
+print(f"Name : {person['name']}")   #gives "Aniket".
+person["age"] = 26
+print(f"age : {person['age']}")
---
+++ objestexercise.py
@@ -0,0 +1,13 @@
+elements = {1,2,3,4,55,6}
+print(f"elemnts: {elements}") 
+
+mylist = {
+    "Apple", "BAnana", "Crocodile", "femboy"
+}
+unique_fruits = set(mylist)
+print(f" unique fruits : {unique_fruits}")
+
+if "banana" in unique_fruits :
+    print(f"banana is inside this {unique_fruits}")
+else:
+    print("time waste")
